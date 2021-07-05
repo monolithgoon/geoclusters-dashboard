6c4c41ebd31629a42967cf85de7cbf2c5584fc60
monolithgoon <svanteahrrenus@hotmail.com>
2021-07-05 11:16:35 +0100

    `stable-commit`

--- server/public/src/js/controllers/login.js
+++ server/public/src/js/controllers/login.js
@@ -4,8 +4,8 @@ export const login = async (email, password) => {
 	try {
 		const res = await axios({
 			method: "POST",
-			// url: "/api/v1/users/login",
-			url: "http://127.0.0.1:1010/api/v1/users/login",
+			url: "/api/v1/users/login",
+			// url: "http://127.0.0.1:1010/api/v1/users/login",
 			data: {
 				user_email: email,
 				user_password: password,
@@ -16,8 +16,8 @@ export const login = async (email, password) => {
 
 			showAlert("success", "Logged In");
 			window.setTimeout(() => {
-				// location.assign("/dashboard");
-				location.assign("https://avg-dashboard.herokuapp.com/dashboard");
+				location.assign("/dashboard");
+				// location.assign("https://avg-dashboard.herokuapp.com/dashboard");
 			}, 1500);
 		};
 
