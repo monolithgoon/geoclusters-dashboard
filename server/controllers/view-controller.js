0c99390f639c7604dbf05635ec5f9c0545e61a67
monolithgoon <svanteahrrenus@hotmail.com>
2021-07-21 11:30:53 +0100

    `removed-deprc-code-in-auth-controller`

--- server/controllers/view-controller.js
+++ server/controllers/view-controller.js
@@ -3,47 +3,6 @@ const chalk = require('../utils/chalk-messages.js');
 const axios = require("axios");
 
 
-// REMOVE > DEPRECATED
-// async function returnLegacyCluster(agc_id) {
-
-// 	try {
-      
-//       const axiosRequest = axios({
-//          method: 'get',
-//          url: `https://geoclusters.herokuapp.com/api/v1/legacy-agcs/`,
-//          crossDomain: true,
-//          responseType: 'application/json',
-//          headers: {
-//             'Accept': '*/*',
-//             'Content-Type': 'application/json',
-//             // 'Authorization': ''
-//          },
-//          data: {
-
-//          }
-//       });
-
-//       // GET RESPONSE FROM API CALL
-//       const apiResponse = await axiosRequest;
-//       const clusterData = JSON.stringify(apiResponse.data);
-//       console.log(clusterData);
-//       return clusterData;
-// 	}
-
-// 	catch (_error) {
-// 		console.error(_error.message);
-// 	};
-// };
-
-// REMOVE > DEPRECATED
-// exports.getAPIData = async (req, res, next) => {
-//    console.log(chalk.success(`called [ getAPIData ] controller fn.`))
-//    req.app.locals.returnedClusters = await returnLegacyCluster();
-//    // console.log(chalk.console(req.app.locals.returnedClusters));
-//    next();
-// }
-
-
 exports.renderAVGDashboard = async (req, res, next) => {
 
    // console.log(chalk.console(JSON.stringify(req.app.locals.geoClusters[0])));
