2882a95bca2999aa6c227c007821f255e40e1d32
monolithgoon <svanteahrrenus@hotmail.com>
2021-08-09 16:57:55 +0100

    `stable-commit`

--- server/controllers/view-controller.js
+++ server/controllers/view-controller.js
@@ -3,11 +3,9 @@ const catchAsync = require('../utils/catch-async.js');
 const chalk = require('../utils/chalk-messages.js');
 
 
-exports.renderAVGDashboard = catchAsync((req, res, next) => {
+exports.renderAVGDashboard = catchAsync(async(req, res, next) => {
 
    console.log(chalk.success(`SUCCESSFULLY CALLED 'renderAVGDashboard' VIEW CONTROLLER FN. `));
-
-   try {
       
       res.status(200).render('dashboard', {
          title: "AVG Dashboard - SSR Alpha",
@@ -17,20 +15,20 @@ exports.renderAVGDashboard = catchAsync((req, res, next) => {
 
       next();
 
-   } catch (renderAGVErr) {
-      console.error(chalk.fail(`renderAGVErr: ${renderAGVErr.message}`));
-   };
-
 }, `renderDashboardErr`);
 
 
-exports.renderLandingPage = catchAsync((req, res, next) => {
-   console.log(chalk.highlight(req.app.locals.clustersSummary))
+exports.renderLandingPage = catchAsync(async(req, res, next) => {
+
    console.log(chalk.success(`SUCCESSFULLY CALLED 'renderLandingPage' VIEW CONTROLLER FN. `));
+
+   console.log(chalk.highlight(JSON.stringify(req.app.locals.clustersSummary)))
+
    res.status(200).render('landing', {
       title: "AGC Platform - SSR V1.0",
       user: "FieldDev Group",
       totalNumClusters: req.app.locals.clustersSummary.totalNumClusters,
       totalNumFeatures: req.app.locals.clustersSummary.totalNumFeatures,
    });
+   
 }, `renderLandingPageErr`);
