e64067c08c89a2c00453bb42f968ccf9b0675242
monolithgoon <svanteahrrenus@hotmail.com>
2022-10-31 08:52:45 +0100

    Modified config.js to read from default.env (dev) and config.eng (prod)

--- server/config/config.js
+++ server/config/config.js
@@ -1,23 +1,28 @@
 const dotenv = require("dotenv"); // read the data from the config file. and use them as env. variables in NODE
-const path = require('path');
-const chalk = require('../utils/chalk-messages.js');
+const path = require("path");
+const chalk = require("../utils/chalk-messages.js");
 
 // set the NODE_ENV to 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 // LOAD THE .env VARIABLES INTO process.env
 if (process.env.NODE_ENV === `development`) {
+	const envConfig = dotenv.config({ path: path.resolve(__dirname, `../default.env`) }); // IMPORTANT > CONFIGURE ENV. VARIABLES BEFORE YOU CALL THE APP
 
-	const envConfig = dotenv.config({path: path.resolve(__dirname, `../config.env`)}) // IMPORTANT > CONFIGURE ENV. VARIABLES BEFORE U CALL THE APP 
-	
 	if (envConfig.error) {
 		// this error should crash the whole process
 		throw new Error(chalk.fail(`Couldn't find .env file. ${envConfig.error}`));
-	};
-};
+	}
+} else {
+	const envConfig = dotenv.config({ path: path.resolve(__dirname, `../config.env`) }); // IMPORTANT > CONFIGURE ENV. VARIABLES BEFORE YOU CALL THE APP
+
+	if (envConfig.error) {
+		// this error should crash the whole process
+		throw new Error(chalk.fail(`Couldn't find .env file. ${envConfig.error}`));
+	}
+}
 
 module.exports = Object.freeze({
-	
 	nodeEnv: process.env.NODE_ENV,
 
 	port: parseInt(process.env.PORT, 10) || 8080,
@@ -26,8 +31,8 @@ module.exports = Object.freeze({
 
 	jwtSecret: process.env.JWT_SECRET,
 	jwtAlgorithm: process.env.JWT_ALGO,
-	jwtExpiresIn: process.env.JWT_EXPIRES_IN,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
-	jwtCookieExpiresIn:process.env.JWT_COOKIE_EXPIRES_IN,
+	jwtExpiresIn: process.env.JWT_EXPIRES_IN,
+	jwtCookieExpiresIn: process.env.JWT_COOKIE_EXPIRES_IN,
 
 	// emails: {
 	// 	apiKey: process.env.MAILGUN_API_KEY,
