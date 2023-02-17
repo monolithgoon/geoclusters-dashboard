2ecc5d72274637445bc75fb1d2bc0b8143ee5031
monolithgoon <svanteahrrenus@hotmail.com>
2023-02-17 10:52:48 +0100

    CI (git-commit-js): adds prompt to push commit to remote master

--- git-commit.js
+++ git-commit.js
@@ -1,46 +1,65 @@
-const { exec } = require('child_process');
-const readline = require('readline');
+const { exec } = require("child_process");
+const readline = require("readline");
 
 // Create a readline interface to prompt the user for a commit message and number of log lines
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+	input: process.stdin,
+	output: process.stdout,
 });
 
 // Prompt the user for a commit message
-rl.question('Enter a commit message: ', (message) => {
+rl.question("ENTER A COMMIT MESSAGE: ", (message) => {
+	// Run the git add and commit commands using the entered message
+	exec(`git add -A && git commit -m "${message}"`, (error, stdout, stderr) => {
+		if (error) {
+			console.error(`Error: ${error.message}`);
+			return;
+		}
+		if (stderr) {
+			console.error(`stderr: ${stderr}`);
+			return;
+		}
+		// console.log(`stdout: ${stdout}`);
 
-  // Run the git add and commit commands using the entered message
-  exec(`git add -A && git commit -m "${message}"`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`stderr: ${stderr}`);
-      return;
-    }
-    // console.log(`stdout: ${stdout}`);
+		// Prompt the user for the number of log lines to show
+		rl.question("ENTER THE NUMBER GIT LOG LINES TO SHOW: ", (numLines) => {
 
-    // Prompt the user for the number of log lines to show
-    rl.question('Enter the number of log lines to show: ', (numLines) => {
+			// Run the git log command using the entered number of lines
+			exec(`git log --oneline -n ${numLines}`, (error, stdout, stderr) => {
 
-      // Run the git log command using the entered number of lines
-      exec(`git log --oneline -${numLines}`, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error: ${error.message}`);
-          return;
-        }
-        if (stderr) {
-          console.error(`stderr: ${stderr}`);
-          return;
-        }
-        // console.log(`Git log (${numLines} lines):\n${stdout}`);
-        console.log(`Git log (${numLines} lines):\n}`);
-      });
+				if (error) {
+					console.error(`Error: ${error.message}`);
+					return;
+				}
+				if (stderr) {
+					console.error(`stderr: ${stderr}`);
+					return;
+				}
 
-      // Close the readline interface
-      rl.close();
-    });
-  });
+				// console.log(`Git log (${numLines} lines):\n${stdout}`);
+				console.log(`Git log (${numLines} lines):\n}`);
+			});
+
+			// Promit the user to push commit to remote origin or not
+			rl.question(`PUSH COMMIT TO REMOTE ORIGIN? (YES | Y or NO | N)`, (response) => {
+
+				// User chooses to commit to remote origin
+				if (response.toLowerCase === "y") {
+
+					exec(`git push origin master`, (error, stdout, stderr) => {
+						if (error) {
+							console.error(`Error: ${error.message}`);
+							return;
+						}
+						if (stderr) {
+							console.error(`stderr: ${stderr}`);
+							return;
+						}
+					});
+				}
+				// Close the readline interface
+				rl.close();
+			});
+		});
+	});
 });
